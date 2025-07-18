3a4f5c18fc26045168f184f66df677ff27ddfa03
arathikr <learnreactstack@gmail.com>
2025-07-18 19:29:24 +0530

    feat(prototypes): javascript prototypes

---
+++ OOJavascriptAndPrototypalInheritance/prototypes.js
@@ -0,0 +1,48 @@
+var person = {
+  firstname: 'Default',
+  lastname: 'Default',
+  getFullName: function () {
+    return this.firstname + ' ' + this.lastname;
+  },
+};
+
+var john = {
+  firstname: 'John',
+  lastname: 'Doe',
+};
+// don't do this EVER! for demo purposes only
+john.__proto__ = person;
+console.log(john.getFullName());
+console.log(john.firstname);
+
+var jane = {
+  firstname: 'Jane',
+};
+
+jane.__proto__ = person;
+
+console.log(jane.getFullName());
+
+// Example of Reflection in Javascript
+
+// loop over every member of object
+for (var prop in john) {
+  if (john.hasOwnProperty(prop)) {
+    console.log(prop + ': ' + john[prop]);
+  }
+}
+
+var jane = {
+  address: '111 Main St.',
+  getFormalFullName: function () {
+    return this.lastname + ', ' + this.firstname;
+  },
+};
+
+var jim = {
+  getFirstName: function () {
+    return firstname;
+  },
+};
+
+// _.extend(john, jane, jim);
